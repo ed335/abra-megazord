c27d3c3d58c579148f51541b1b001169e908d757
abracann <abracann@gmail.com>
2025-12-11 00:06:29 -0300

    Ajusta grid de beneficios para 2 colunas

--- web/components/home/BenefitsSection.tsx
+++ web/components/home/BenefitsSection.tsx
@@ -25,7 +25,7 @@ export default function BenefitsSection() {
           </h2>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 sm:gap-6">
           {benefits.map((benefit, index) => (
             <motion.div
               key={benefit.title}
