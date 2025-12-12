d6608306147a539bf8bea98c095913d27ca07d2c
abracann <abracann@gmail.com>
2025-12-12 03:59:38 -0300

    Add robots.txt and sitemap routes

---
+++ web/app/robots.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from "next";
+
+const baseUrl = "https://abracann.com";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
+  };
+}
